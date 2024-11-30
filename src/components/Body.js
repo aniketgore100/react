86593893582a856f7b42b1bc7aafac6bb63b9496
Aniket <goreaniket100@gmail.com>
2024-11-30 23:10:07 +0530

    states and props with search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,47 @@
-import {restList} from "../config"
+import { useState } from "react";
+import { restList } from "../config";
 import RestCard from "./RestCard";
 
+function filterData(searchTxt, List) {
+  const filterdata = List.filter( (item)=>item.name.toLowerCase().includes(searchTxt.toLowerCase()));
+  return filterdata;
+}
 
 const Body = () => {
-    return (
+  const [searchTxt, SetSearchTxt] = useState("");
+  const [TotalRes, setTotalRes] = useState(restList);
+
+  
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          className="search"
+          placeholder="search"
+          value={searchTxt}
+          onChange={(e) => {
+            SetSearchTxt(e.target.value);
+          }}
+        />
+        <button
+          className="button"
+          onClick={() => {
+            const data = filterData(searchTxt, restList);
+            setTotalRes(data);
+          }}
+        >
+          search
+        </button>
+      </div>
+
       <div className="restoCard">
-        {restList.map((rest) => {
+        {TotalRes.map((rest) => {
           return <RestCard {...rest} />;
         })}
       </div>
-    );
-  };
+    </>
+  );
+};
 
-  export default Body;
+export default Body;
